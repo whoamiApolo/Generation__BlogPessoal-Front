c8418de61a4d0d647bcce4e037cee94a1c85f561
Apolinario <whoami.apolo@gmail.com>
2023-08-11 08:47:38 -0300

    att arquivos para o deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
 import Login from './pages/login/Login';
